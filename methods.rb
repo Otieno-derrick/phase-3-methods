c16372937f79ec962a53603ccb754b95da699461
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-08-15 10:12:11 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer()
+
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+
+def greet_with_default(name = "programmer") 
+    puts "Hello, #{name}!"
+end
+greet_with_default("Sunny")
+
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+sum = add(2,5)
+
+
+
+def halve(number)
+    return  nil if number.class != Integer
+    return 3 if number / 2
+end
